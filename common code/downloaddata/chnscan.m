12c75e02148a107e85fe301be546b01307f5e806
lijiajia001 <156407350+lijiajia001@users.noreply.github.com>
2024-01-16 16:39:12 +0800

    comment

--- common code/downloaddata/chnscan.m
+++ common code/downloaddata/chnscan.m
@@ -1,5 +1,5 @@
 function  Chnl=chnscan(shotnum)
-% This function is used to 
+% This function is used to download the channels from mdsplus database
 IpAddress='192.168.20.11';
 treeNames={'exl50u'};
 fullPath = mfilename('fullpath');
